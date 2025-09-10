e39b532d49b86db7be2d640d6bb02b4f30584b46
Venkat <venkatjeeva248@gmail.com>
2025-09-10 17:29:27 +0530

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,27 @@
+d={"Eno":3872,"Ename":"naruto"}
+print(d)
+print(d["Eno"])
+print(d.get["Ename"])":123,"Ename":"abc" }
+d["Ename"]="venkat"
+print(d)
+d=[age]=19
+print(d)
+for key in d:
+    print(key)
+for value in d.values():
+    print(value)
+for key,values in d.items():
+    print(key,values)
+d.pop("Ename")
+print(d)
+d.popitem()
+print(d)
+d.clear()
+print(d)
+
+
+
+
+
+
+
